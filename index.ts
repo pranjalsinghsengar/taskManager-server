f8ee4d38580c2865e899cf2e18c103d87210a9dc
pranjal singh sengar <67635283+pranjalsinghsengar@users.noreply.github.com>
2025-02-20 14:05:05 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config();
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({ origin: '*', credentials: true }));
