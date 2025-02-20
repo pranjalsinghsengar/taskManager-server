fe2509ca27226d3c0f017bfb427f4dbc10643c1e
pranjal singh sengar <67635283+pranjalsinghsengar@users.noreply.github.com>
2025-02-20 12:39:13 +0530

    Update index.ts

--- src/Task/router/index.ts
+++ src/Task/router/index.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
-import { CreateTaskController, TaskListController } from "../controller";
+import {
+  CreateTaskController,
+  TaskListController,
+  DeleteTaskController,
+  UpdateTaskController,
+  MaskUpdateTaskController,
+} from "../controller";
 import { JWT_Validation } from "../../middleware/jwtValidation";
 
-const taskRouter = Router()
+const taskRouter = Router();
 
-taskRouter.post("/create", JWT_Validation, CreateTaskController)
-taskRouter.get("/list", JWT_Validation, TaskListController)
+taskRouter.post("/create", JWT_Validation, CreateTaskController);
+taskRouter.get("/list", JWT_Validation, TaskListController);
+taskRouter.get("/update/status", JWT_Validation, MaskUpdateTaskController);
+taskRouter.get("/update", JWT_Validation, UpdateTaskController);
+taskRouter.get("/update", JWT_Validation, DeleteTaskController);
 
-
-export default taskRouter
+export default taskRouter;
